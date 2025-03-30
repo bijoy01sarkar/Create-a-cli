bbc16a1ca2e1f23a2e4c475c317b40dc08a05e4d
Bijoy Sarkar <75505462+bijoy01sarkar@users.noreply.github.com>
2025-03-30 08:15:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
 Create a `command line interface` that lets the user specify a file path and the nodejs process counts the number of words inside it.
 
-Input - node index.js /Users/kirat/file.txt
+Input - node index.js /Users/bijoy/file.txt
 Output - You have 10 words in this file
 
 Command - `node index.js count_words filePath`
